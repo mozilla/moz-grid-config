2bf9d8353243d2654a3e928ac1a4b63a4bc52a01
Bebe <florin.strugariu@softvision.ro>
2012-07-25 15:55:00 +0100

    Remove support for Selenium RC custom profiles

--- firefoxprofiles/certificateExceptions/prefs.js
+++
@@ -1,7 +0,0 @@
-user_pref("javascript.options.jitprofiling.chrome", false);
-user_pref("javascript.options.jitprofiling.content", false);
-user_pref("javascript.options.methodjit.chrome", false);
-user_pref("javascript.options.methodjit.content", false);
-user_pref("javascript.options.methodjit_always", false);
-user_pref("javascript.options.tracejit.chrome", false);
-user_pref("javascript.options.tracejit.content", false);
